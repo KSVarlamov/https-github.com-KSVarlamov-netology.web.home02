5df248b5aebbbdfa0ce16f1c99b536c2ef788079
KSVarlamov <varlamov.ks@mail.ru>
2023-03-20 17:39:53 +0300

    Correction of remarks

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -6,11 +6,12 @@ import ru.netology.model.Post;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class PostRepository {
 
     private static final ConcurrentHashMap<Long, Post> data = new ConcurrentHashMap<>();
-    private static volatile long idCounter = 1;
+    private static final AtomicLong idCounter = new AtomicLong(1L);
 
     public List<Post> all() {
         return data.values().parallelStream().toList();
@@ -20,13 +21,13 @@ public class PostRepository {
         return Optional.ofNullable(data.get(id));
     }
 
-    public synchronized Post save(Post post) {
+    public Post save(Post post) {
         if (post.getId() == 0) {
-            post.setId(idCounter);
-            idCounter++;
+            post.setId(idCounter.get());
+            idCounter.incrementAndGet();
             data.put(post.getId(), post);
         } else {
-            Post tmp = data.get(post.getId());
+            var tmp = data.get(post.getId());
             if (tmp != null) {
                 data.put(post.getId(), post);
             } else {
