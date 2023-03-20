72efbf201ff9fb71e3d4c858d9aa3be81e3366af
KSVarlamov <varlamov.ks@mail.ru>
2023-03-20 12:08:20 +0300

    Task 1 resolved

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,45 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
-// Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private static final ConcurrentHashMap<Long, Post> data = new ConcurrentHashMap<>();
+    private static volatile long idCounter = 1;
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        return data.values().parallelStream().toList();
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    public synchronized Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(idCounter);
+            idCounter++;
+            data.put(post.getId(), post);
+        } else {
+            Post tmp = data.get(post.getId());
+            if (tmp != null) {
+                data.put(post.getId(), post);
+            } else {
+                throw new NotFoundException();
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        var tmp = data.remove(id);
+        if (tmp == null) {
+            throw new NotFoundException(id + " not found");
+        }
+    }
 }
