314ebf05035227560bcade7cf84a31483c97221b
KSVarlamov <varlamov.ks@mail.ru>
2023-03-24 14:04:31 +0300

    Adding DTO

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -2,32 +2,37 @@ package ru.netology.controller;
 
 import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
+import ru.netology.model.PostDTO;
 import ru.netology.service.PostService;
+import ru.netology.utils.MapUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService service;
-
-    public PostController(PostService service) {
+private final MapUtils mapUtils;
+    public PostController(PostService service, MapUtils mapUtils) {
         this.service = service;
+        this.mapUtils = mapUtils;
     }
 
     @GetMapping
-    public List<Post> all() {
-        return service.all();
+    public List<PostDTO> all() {
+        return service.all().stream().map(mapUtils::postToPostDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public Post getById(@PathVariable long id) {
-        return service.getById(id);
+    public PostDTO getById(@PathVariable long id) {
+        return mapUtils.postToPostDTO(service.getById(id));
     }
 
     @PostMapping
-    public Post save(@RequestBody Post post) {
-        return service.save(post);
+    public PostDTO save(@RequestBody PostDTO post) {
+        Post result = service.save(mapUtils.postDtoToPost(post));
+        return mapUtils.postToPostDTO(result);
     }
 
     @DeleteMapping("/{id}")
